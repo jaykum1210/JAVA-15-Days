be45e5599a99b1e89607494420eeb714a98b5e56
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-02 20:23:58 +0530

    add files

---
+++ 5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class tri3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = a - i - 1; j >= 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
