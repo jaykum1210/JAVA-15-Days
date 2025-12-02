e808355c3859d02b0ea31d0f824d728458091b0e
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-02 20:39:10 +0530

    add file

---
+++ 6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class tri4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i = 0;i<a;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
