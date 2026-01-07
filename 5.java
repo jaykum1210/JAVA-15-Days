53addf0e65274631e0b7ba5bebdda5a3e9809dc8
jaykum1210 <jaykumawat1210@gmail.com>
2026-01-07 12:45:37 +0530

    add files

--- 5.java
+++ 5.java
@@ -9,7 +9,7 @@ class tri3 {
                 System.out.print(" ");
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println(" ");
         }
